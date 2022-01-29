b43c6cafb3646c71b35563b09b00b2e299477ec7
kekkerslawl <braydj@gmail.com>
2022-01-29 09:16:48 -0600

    finished LogLevels

--- java/log-levels/src/main/java/LogLevels.java
+++ java/log-levels/src/main/java/LogLevels.java
@@ -1,14 +1,21 @@
 public class LogLevels {
     
     public static String message(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.message() method");
+      String result[] = logLine.split(":");
+      String message = result[1];
+      return message.strip();
+      
     }
 
     public static String logLevel(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.logLevel() method");
+      
+      String result[] = logLine.split(":");
+      String level = result[0];
+      return level.replaceAll("\\W", "").toLowerCase();
+
     }
 
-    public static String reformat(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.reformat() method");
+    public static String reformat(String logLine) {      
+      return LogLevels.message(logLine).concat(" (" + LogLevels.logLevel(logLine) + ")");
     }
 }
--- java/log-levels/src/test/java/LogLevelsTest.java
+++ java/log-levels/src/test/java/LogLevelsTest.java
@@ -1,4 +1,5 @@
 import org.junit.Test;
+import org.junit.Ignore;
 
 import static org.assertj.core.api.Assertions.*;
 
