594ae3034db21b87fab58952147573f15e492079
kekkerslawl <braydj@gmail.com>
2022-01-29 14:33:35 -0600

    finished Blackjack

---
+++ java/blackjack/src/main/java/Blackjack.java
@@ -0,0 +1,90 @@
+public class Blackjack {
+
+  public int parseCard(String card) {
+    int val = 0;
+    switch (card) {
+      case "ace":
+        val = 11;
+        break;
+      case ("ten"):
+      case ("king"):
+      case ("queen"):
+      case ("jack"):
+        val = 10;
+        break;
+      case ("nine"):
+        val = 9;
+        break;
+      case ("eight"):
+        val = 8;
+        break;
+      case ("seven"):
+        val = 7;
+        break;
+      case ("six"):
+        val = 6;
+        break;
+      case ("five"):
+        val = 5;
+        break;
+      case ("four"):
+        val = 4;
+        break;
+      case ("three"):
+        val = 3;
+        break;
+      case ("two"):
+        val = 2;
+        break;
+    }
+    return val;
+  }
+
+  public boolean isBlackjack(String card1, String card2) {
+    if ((parseCard(card1) + parseCard(card2)) == 21) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public String largeHand(boolean isBlackjack, int dealerScore) {
+    if (!isBlackjack) {
+      return "P";
+    }
+    if (isBlackjack && dealerScore >= 10) {
+      return "S";
+    } else {
+      return "W";
+    }
+
+  }
+
+  public String smallHand(int handScore, int dealerScore) {
+    if (handScore >= 17) {
+      return "S";
+    } else if (handScore <= 11) {
+      return "H";
+    } else if ((16 >= handScore && handScore >= 12) && dealerScore >= 7) {
+      return "H";
+    } else {
+      return "S";
+    }
+  }
+
+  // FirstTurn returns the semi-optimal decision for the first turn, given the
+  // cards of the player and the dealer.
+  // This function is already implemented and does not need to be edited. It pulls
+  // the other functions together in a
+  // complete decision tree for the first turn.
+  public String firstTurn(String card1, String card2, String dealerCard) {
+    int handScore = parseCard(card1) + parseCard(card2);
+    int dealerScore = parseCard(dealerCard);
+
+    if (20 < handScore) {
+      return largeHand(isBlackjack(card1, card2), dealerScore);
+    } else {
+      return smallHand(handScore, dealerScore);
+    }
+  }
+}
