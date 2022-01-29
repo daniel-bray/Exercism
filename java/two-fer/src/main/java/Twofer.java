cc77abba05bab4dfc2de19113fada0687701a8c0
kekkerslawl <braydj@gmail.com>
2022-01-29 09:17:33 -0600

    finished LogLevels

--- java/two-fer/src/main/java/Twofer.java
+++ java/two-fer/src/main/java/Twofer.java
@@ -1,8 +1,10 @@
 public class Twofer {
     public String twofer(String name) {
-    if (name == null){
-      name = "you";
-    }
-       return "One for " + name + ", one for me.";
+    // if (name == null){
+    //   name = "you";
+    // }
+    // use ternary instead of if statement
+    String personsName = name==null?"you":name;
+       return "One for " + personsName + ", one for me.";
     }
 }
