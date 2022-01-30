5b943c94243aa20221aa765eb19bcc0bbc6dbe1a
kekkerslawl <braydj@gmail.com>
2022-01-30 10:23:04 -0600

    finished BirdWatcher

---
+++ java/bird-watcher/src/main/java/BirdWatcher.java
@@ -0,0 +1,58 @@
+
+class BirdWatcher {
+    private final int[] birdsPerDay;
+
+    public BirdWatcher(int[] birdsPerDay) {
+        this.birdsPerDay = birdsPerDay.clone();
+    }
+
+    public int[] getLastWeek() {
+        int[] lastWeek = new int[] {0,2,5,3,7,8,4};
+        return lastWeek;
+}
+
+    public int getToday() {
+        if (birdsPerDay.length == 0){
+          return 0;
+    } else {
+
+        return birdsPerDay[birdsPerDay.length - 1];
+    }
+    }
+
+    public void incrementTodaysCount() {
+        birdsPerDay[birdsPerDay.length - 1]++;
+    }
+
+    public boolean hasDayWithoutBirds() {
+        boolean hasNone = false;
+        for(int i = 0; i<birdsPerDay.length;i++){
+          if(birdsPerDay[i] == 0){
+            hasNone = true;
+          }
+      }
+      return hasNone;
+    }
+
+    public int getCountForFirstDays(int numberOfDays) {
+        int sum = 0;
+        if (numberOfDays > birdsPerDay.length){
+          numberOfDays = birdsPerDay.length;
+        }
+        for (int i = 0; i < numberOfDays; i++){
+          sum+= birdsPerDay[i];
+        }
+
+        return sum;
+    }
+
+    public int getBusyDays() {
+        int busyCount = 0;
+        for (int i = 0; i < birdsPerDay.length; i++){
+          if (birdsPerDay[i] >= 5){
+            busyCount++;
+          }
+        }
+        return busyCount;
+    }
+}
---
+++ java/squeaky-clean/src/main/java/SqueakyClean.java
@@ -0,0 +1,28 @@
+class SqueakyClean {
+    static String clean(String identifier) {
+        // throw new UnsupportedOperationException("Please implement the (static) SqueakyClean.clean() method");
+        String test = identifier;
+        test = test.replaceAll(" ", "_");
+
+        StringBuilder sb = new StringBuilder(test);
+        
+        for (int i = 0; i < test.length(); i++){
+            // if(Character.isDigit(test.charAt(i))){
+                // sb.replace(i, i+1, "");
+            // }
+            if (Character.isISOControl(test.charAt(i))){
+                sb.replace(i, i+1, "CTRL");
+            } 
+            if (Character.getType(test.charAt(i)) == Character.DASH_PUNCTUATION){
+                  StringBuilder upperCaseLetter = new StringBuilder();
+                upperCaseLetter.append(Character.toUpperCase(test.charAt(i+1)));
+                sb.replace(i, i+2, upperCaseLetter.toString());
+            }
+if (!Character.isLetter(test.charAt(i))){
+                sb.replace(i, i+1, "");
+            }
+        }
+        return sb.toString();
+    }
+    
+}
