813450681ab653f75d8a7d7d32f98ea28b240b31
kekkerslawl <braydj@gmail.com>
2022-01-30 11:17:11 -0600

    finished KarlsLanguages

--- java/bird-watcher/src/main/java/BirdWatcher.java
+++ java/bird-watcher/src/main/java/BirdWatcher.java
@@ -1,58 +1,58 @@
 
 class BirdWatcher {
-    private final int[] birdsPerDay;
+  private final int[] birdsPerDay;
 
-    public BirdWatcher(int[] birdsPerDay) {
-        this.birdsPerDay = birdsPerDay.clone();
-    }
+  public BirdWatcher(int[] birdsPerDay) {
+    this.birdsPerDay = birdsPerDay.clone();
+  }
 
-    public int[] getLastWeek() {
-        int[] lastWeek = new int[] {0,2,5,3,7,8,4};
-        return lastWeek;
-}
+  public int[] getLastWeek() {
+    int[] lastWeek = new int[] { 0, 2, 5, 3, 7, 8, 4 };
+    return lastWeek;
+  }
 
-    public int getToday() {
-        if (birdsPerDay.length == 0){
-          return 0;
+  public int getToday() {
+    if (birdsPerDay.length == 0) {
+      return 0;
     } else {
 
-        return birdsPerDay[birdsPerDay.length - 1];
+      return birdsPerDay[birdsPerDay.length - 1];
     }
-    }
+  }
 
-    public void incrementTodaysCount() {
-        birdsPerDay[birdsPerDay.length - 1]++;
-    }
+  public void incrementTodaysCount() {
+    birdsPerDay[birdsPerDay.length - 1]++;
+  }
 
-    public boolean hasDayWithoutBirds() {
-        boolean hasNone = false;
-        for(int i = 0; i<birdsPerDay.length;i++){
-          if(birdsPerDay[i] == 0){
-            hasNone = true;
-          }
+  public boolean hasDayWithoutBirds() {
+    boolean hasNone = false;
+    for (int i = 0; i < birdsPerDay.length; i++) {
+      if (birdsPerDay[i] == 0) {
+        hasNone = true;
       }
-      return hasNone;
+    }
+    return hasNone;
+  }
+
+  public int getCountForFirstDays(int numberOfDays) {
+    int sum = 0;
+    if (numberOfDays > birdsPerDay.length) {
+      numberOfDays = birdsPerDay.length;
+    }
+    for (int i = 0; i < numberOfDays; i++) {
+      sum += birdsPerDay[i];
     }
 
-    public int getCountForFirstDays(int numberOfDays) {
-        int sum = 0;
-        if (numberOfDays > birdsPerDay.length){
-          numberOfDays = birdsPerDay.length;
-        }
-        for (int i = 0; i < numberOfDays; i++){
-          sum+= birdsPerDay[i];
-        }
+    return sum;
+  }
 
-        return sum;
+  public int getBusyDays() {
+    int busyCount = 0;
+    for (int i = 0; i < birdsPerDay.length; i++) {
+      if (birdsPerDay[i] >= 5) {
+        busyCount++;
+      }
     }
-
-    public int getBusyDays() {
-        int busyCount = 0;
-        for (int i = 0; i < birdsPerDay.length; i++){
-          if (birdsPerDay[i] >= 5){
-            busyCount++;
-          }
-        }
-        return busyCount;
-    }
+    return busyCount;
+  }
 }
---
+++ java/karls-languages/src/main/java/LanguageList.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LanguageList {
+  private final List<String> languages = new ArrayList<>();
+
+  public boolean isEmpty() {
+    if (languages.isEmpty()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public void addLanguage(String language) {
+    languages.add(language);
+  }
+
+  public void removeLanguage(String language) {
+    languages.remove(language);
+  }
+
+  public String firstLanguage() {
+    return languages.get(0);
+  }
+
+  public int count() {
+    return languages.size();
+  }
+
+  public boolean containsLanguage(String language) {
+    if (languages.contains(language)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public boolean isExciting() {
+    boolean hasJava = languages.contains("Java");
+    boolean hasKotlin = languages.contains("Kotlin");
+    if (hasJava || hasKotlin) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
