0e4b2ffea6d3c70e18077ccf8983931eecfaa46b
kekkerslawl <braydj@gmail.com>
2022-02-01 13:45:28 -0600

    finished SqueakyClean

--- java/squeaky-clean/src/main/java/SqueakyClean.java
+++ java/squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,25 +1,22 @@
 class SqueakyClean {
   static String clean(String identifier) {
-    // throw new UnsupportedOperationException("Please implement the (static)
-    // SqueakyClean.clean() method");
-    String test = identifier;
-    test = test.replaceAll(" ", "_");
 
-    StringBuilder sb = new StringBuilder(test);
-
-    for (int i = 0; i < test.length(); i++) {
-
-      if (Character.isISOControl(test.charAt(i))) {
-        sb.replace(i, i + 1, "CTRL");
-      }
-      if (Character.getType(test.charAt(i)) == Character.DASH_PUNCTUATION) {
-        StringBuilder upperCaseLetter = new StringBuilder();
-        upperCaseLetter.append(Character.toUpperCase(test.charAt(i + 1)));
-        sb.replace(i, i + 2, upperCaseLetter.toString());
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < identifier.length(); i++) {
+      char ch = identifier.charAt(i);
+      if (Character.isSpaceChar(ch)) {
+        sb.append("_");
+      } else if (Character.isISOControl(ch)) {
+        sb.append("CTRL");
+      } else if (ch == '-' && i < identifier.length() + 1) {
+        i++;
+        sb.append(Character.toUpperCase(identifier.charAt(i)));
+      } else if (ch >= '\u03b1' && ch <= '\u03c9') {
+      } else if (Character.isAlphabetic(ch)) {
+        sb.append(ch);
       }
 
     }
     return sb.toString();
   }
-
 }
